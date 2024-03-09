261d49e1693f651539e987bd52837ad7e6074ef9
Vadim Budarin <budarin-vv@yandex.ru>
2024-03-09 16:59:00 +0300

    fix client setting

--- scripts/client.sh
+++ scripts/client.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-tsc ./src/client/index.ts --outDir ./dist/client;
+tsc --project client.tsconfig.json;
 cp ./src/index.html ./dist/client;
 http-server ./dist/client -p 3000 -b -g --cors
